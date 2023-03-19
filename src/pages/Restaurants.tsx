14c8803864cf19f50cd8b728edd198c9567b4f0d
azizavant <106331007+azizavant@users.noreply.github.com>
2023-03-20 01:30:52 +0600

    displaying owners all restaurants in home page

--- src/pages/Restaurants.tsx
+++ src/pages/Restaurants.tsx
@@ -10,6 +10,7 @@ export const Restaurants = () => {
     const restaurants = useSelector<AppStateType, RestaurantType[]>(state => state.auth.restaurants)
     const user = useSelector<AppStateType, UserType>(state => state.auth.user)
 
+
     const onClickRestaurant = (id: number) => {
         navigate(`/home/restaurants/${id}`)
     }
@@ -21,14 +22,23 @@ export const Restaurants = () => {
     }, [user]);
 
     return (
-        <div>
+        <div className={'flex items-center flex-col h-full'}>
             <h1>All you restaurants</h1>
-            <div>
+            <div className={"w-full bg-white flex flex-col mt-6 p-6 rounded-3xl"}>
                 {restaurants.map(r => {
-                    return <div key={r.id} className="flex gap-7 bg-white mb-0 mx-auto px-7 py-6 rounded-2xl shadow-[0_22px_47px_rgba(28,82,121,0.22)]">
-                        <h2>{r.title}</h2>
-                        <button className="btn" onClick={() => onClickRestaurant(r.id)}>go to</button>
-                    </div>
+                    return (
+                        <div key={r.id}
+                             onClick={() => onClickRestaurant(r.id)}
+                             className={"relative flex justify-center items-center last:mb-0 mb-7 h-[180px] w-full gap-7 bg-white mb-0 mx-auto px-0 py-0 rounded-2xl shadow-[0_22px_47px_rgba(28,82,121,0.22)]"}
+                        >
+                            <img className={'h-full w-full brightness-[45%] object-cover rounded-xl'}
+                                 src="https://images.unsplash.com/photo-1574071318508-1cdbab80d002?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80"
+                                 alt="restaurant image"/>
+                            <h1 className={'absolute text-[22px] text-[white] h-[19px] w-[230px] flex items-center justify-center text-center'}>
+                                {r.title.toUpperCase()}
+                            </h1>
+                        </div>
+                    )
                 })}
             </div>
         </div>
